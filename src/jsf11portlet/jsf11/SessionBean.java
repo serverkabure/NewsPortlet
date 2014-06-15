f6e4c5a2138e59aa7613a984b0f2585d76e4abed
serverkabure <serverkabure@live.jp>
2014-06-16 05:17:48 +0900

    jsf11-ok

---
+++ src/jsf11portlet/jsf11/SessionBean.java
@@ -0,0 +1,29 @@
+package jsf11portlet.jsf11;
+
+import java.io.Serializable;
+
+public class SessionBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private int number = 0;
+
+	public SessionBean() {
+	}
+
+	public void countUp() {
+		this.number++;
+	}
+
+	public void countDown() {
+		this.number--;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+}
